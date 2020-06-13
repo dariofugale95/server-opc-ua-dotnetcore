c04b99788927b1228c995ee1b98fbba7f92fadd6
Dario Fugale <uni357707@studium.unict.it>
2020-06-13 11:44:39 +0200

    MyOPCServer.cs - class which implements an instance of a UA server

--- OPCServerNETCore/OPCServerNETCore/OPCServer/MyOPCServer.cs
+++ OPCServerNETCore/OPCServerNETCore/OPCServer/MyOPCServer.cs
@@ -2,6 +2,8 @@
 using Opc.Ua.Server;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
 using System.Text;
 
 namespace OPCServerNETCore.OPCServer
@@ -12,17 +14,40 @@ namespace OPCServerNETCore.OPCServer
         public MyOPCServer()
         {
         }
+
+
         #endregion
 
         #region Overridden Methods
-        protected override void OnNodeManagerStarted(IServerInternal server)
+        protected override MasterNodeManager CreateMasterNodeManager(IServerInternal server, ApplicationConfiguration configuration)
         {
-            base.OnNodeManagerStarted(server);
+            List<INodeManager> managers = new List<INodeManager>();
+
+            managers.Add(new MyOPCServerNodeManager(server, configuration));
+
+            return new MasterNodeManager(server, configuration, null, managers.ToArray());
+        }
+        protected override ServerProperties LoadServerProperties()
+        {
+            var thisAssembly = GetType().Assembly;
+            ServerProperties properties = new ServerProperties();
+
+            properties.ManufacturerName = "castagnolofugale";
+            properties.ProductName = "MyOPCServer";
+            properties.ProductUri = "https://github.com/dariofugale95/server-opc-ua-dotnetcore";
+            properties.SoftwareVersion = thisAssembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
+            properties.BuildNumber = thisAssembly.GetCustomAttribute<AssemblyFileVersionAttribute>().Version;
+            properties.BuildDate = File.GetLastWriteTimeUtc(thisAssembly.Location);
+
+            return properties;
         }
 
         protected override void OnServerStarted(IServerInternal server)
         {
             base.OnServerStarted(server);
+
+            // request notifications when the user identity is changed. all valid users are accepted by default.
+            // server.SessionManager.ImpersonateUser += new ImpersonateEventHandler(SessionManager_ImpersonateUser);
         }
 
         protected override void OnServerStarting(ApplicationConfiguration configuration)
@@ -30,10 +55,6 @@ namespace OPCServerNETCore.OPCServer
             base.OnServerStarting(configuration);
         }
 
-        protected override void OnServerStopping()
-        {
-            base.OnServerStopping();
-        }
         #endregion
     }
 }
